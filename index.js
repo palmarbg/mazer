d840e8467f9ead9a05f356d43aabadf0ce8d8d82
Márton Páll <s2n5o1@INF.ELTE.HU>
2024-04-06 16:44:10 +0200

    Remove documentation from code

--- index.js
+++ index.js
@@ -1,38 +1,16 @@
 import labyrinthos from 'labyrinthos';
 import * as utils from './utils.js'
 
-/*
-GENERATORS:
-
-labyrinthos.mazes.
-    AldousBroder
-    BinaryTree
-    CellularAutomata
-    EllersAlgorithm
-    GrowingTree
-    RecursiveBacktrack
-    RecursiveDivision
-    ThomasHunter
-    BeattieSchoberth
-    Metroidvania
-
-FOR EMPTY MAP:
-(map,_) => map.fill(0)
-
-*/
-
-
-
 const options = {
-    mapWidth: 150,
+    mapWidth: 250,
     mapHeight: 100,
     seed: Math.floor(Math.random()*999999999),
     generator: labyrinthos.mazes.Metroidvania,
-    generatorOptions: {retries:10}, // generatorOptions: {}
+    generatorOptions: {retries:10},
     filter: null,
-    numberOfTasks: 2000,
-    onlyUniqueTasks: false,
-    numberOfAgents: 40,
+    numberOfTasks: 100,
+    onlyUniqueTasks: true,
+    numberOfAgents: 20,
 }
 
 console.log('generatorOptions: ', options)
@@ -51,53 +29,3 @@ utils.generateMap()
 
 utils.generateTasks(options.numberOfTasks, options.onlyUniqueTasks)
 utils.generateAgents(options.numberOfAgents)
-
-
-
-/*
-GENERATOR OPTIONS
-
-CellularAutomata
-    options.wallChance || 0.45
-    options.iterations || 4
-    options.neighborThreshold || 4
-ThomasHunter
-    options.roomCount
-    options.roomMinWidth
-    options.roomMaxWidth
-    options.roomMinHeight
-    options.roomMinHeight
-BeattieSchoberth
-    options.roomHeight
-    options.roomWidth
-Metroidvania
-    options.retries || 4 (4 is a safe number, 5-8 is fine, 10 can start to pause slower systems)
-*/
-
-/*
-EXAMPLE
-
-options = {
-    mapWidth: 150,
-    mapHeight: 100,
-    seed: Math.floor(Math.random()*999999999),
-    generator: labyrinthos.mazes.Metroidvania,
-    generatorOptions: {retries:10}, // generatorOptions: {}
-    filter: null,
-    numberOfTasks: 2000,
-    onlyUniqueTasks: false,
-    numberOfAgents: 40,
-}
-
-options = {
-    mapWidth: 160,
-    mapHeight: 20,
-    seed: 12,
-    generator: labyrinthos.mazes.CellularAutomata,
-    generatorOptions: {wallChance:0.3,iterations:2,neighborThreshold:3},
-    filter: null,
-    numberOfTasks: 2000,
-    onlyUniqueTasks: false,
-    numberOfAgents: 40,
-}
-*/
